2f4ccc2cf17e3c7b384574054fb0d33be94e4532
Mohammad Rayan <mohammad.rayan@radcom.com>
2019-05-08 14:42:37 +0300

    changed the name

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-mohammad-rayan",
+    name="example",
     version="0.0.1",
     author="Example Author",
     author_email="rayan.m28@gmail.com",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/rayanm28/example_py2",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
